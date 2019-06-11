e2af99aa4058754cffa0f12e0f108c0659dc89a5
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-11 11:14:15 +0300

    Integrate and configure Oscar.

--- oscar_multicurrency/urls.py
+++ oscar_multicurrency/urls.py
@@ -1,6 +1,8 @@
+from django.apps import apps
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(apps.get_app_config('oscar').urls[0]))
 ]
