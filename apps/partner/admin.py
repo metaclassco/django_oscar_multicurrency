c3a462130a38a34fb286ddb311cc9fe0676586e4
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-11 13:34:01 +0300

    Implement basic currency model and model admin.

--- apps/partner/admin.py
+++ apps/partner/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from .models import Currency
+
+
+admin.site.register(Currency)
+
+
 from oscar.apps.partner.admin import *  # noqa
--- apps/partner/models.py
+++ apps/partner/models.py
@@ -1,1 +1,15 @@
+from django.db import models
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=3)
+
+    class Meta:
+        verbose_name_plural = 'Currencies'
+
+    def __str__(self):
+        return self.name
+
+
 from oscar.apps.partner.models import *  # noqa isort:skip
