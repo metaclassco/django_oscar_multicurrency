acde843a92d793e8b6e0b241430098dc37d56eed
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-12 19:08:44 +0300

    Implement basic model admin for exchange rate model.

--- apps/partner/admin.py
+++ apps/partner/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Currency
+from .models import Currency, ExchangeRate
 
 
+class ExchangeRateAdmin(admin.ModelAdmin):
+    list_display = ('base_currency', 'currency', 'value', 'date_created')
+
+
+admin.site.register(ExchangeRate, ExchangeRateAdmin)
 admin.site.register(Currency)
 
 
