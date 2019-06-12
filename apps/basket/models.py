d91dea7bed28fe5f78cb619c8e2f67715b98f201
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-12 14:50:59 +0300

    Make basket currency nullable.

--- apps/basket/models.py
+++ apps/basket/models.py
@@ -3,7 +3,9 @@ from oscar.apps.basket.abstract_models import AbstractBasket
 
 
 class Basket(AbstractBasket):
-    currency = models.ForeignKey('partner.Currency', on_delete=models.CASCADE, related_name='baskets')
+    currency = models.ForeignKey(
+        'partner.Currency', on_delete=models.CASCADE, related_name='baskets', null=True, blank=True
+    )
 
 
 from oscar.apps.basket.models import *  # noqa isort:skip
