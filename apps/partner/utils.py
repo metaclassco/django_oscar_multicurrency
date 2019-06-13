799c8c8021d633c82a5a26893a7f0167aa3eed72
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-13 16:22:41 +0300

    Drop default currency determination, since we return to original Oscar setting after latest experiment.

--- apps/partner/utils.py
+++ apps/partner/utils.py
@@ -2,9 +2,7 @@ from decimal import Decimal as D, ROUND_HALF_UP
 
 import requests
 
-from django.core.cache import cache
-
-from .models import Currency, ExchangeRate
+from .models import ExchangeRate
 
 
 EXCHANGERATES_API_URL = 'https://api.exchangeratesapi.io/latest?base=%s&symbols=%s'
@@ -16,18 +14,6 @@ def fetch_exchange_rates(base_currency, currencies):
     r = requests.get(url)
     if r.status_code == 200:
         return r.json()
-
-
-def get_default_currency():
-    return Currency.objects.first()
-
-
-def get_default_currency_code():
-    default_currency = cache.get(DEFAULT_CURRENCY_CACHE_KEY)
-    if not default_currency:
-        default_currency = get_default_currency()
-        cache.set(DEFAULT_CURRENCY_CACHE_KEY, default_currency)
-    return default_currency
 
 
 def convert_currency(from_currency, to_currency, value):
