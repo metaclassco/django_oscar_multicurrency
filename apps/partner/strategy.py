4233b0cd861059410d503517218e546a0458cec9
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-13 16:27:39 +0300

    Refactor partner strategy since we got rid from currency model, do not try to convert money if product does not have stockrecord.

--- apps/partner/strategy.py
+++ apps/partner/strategy.py
@@ -1,8 +1,11 @@
 from decimal import Decimal as D
+
+from django.conf import settings
+
 from oscar.apps.partner.strategy import Default as CoreDefault
 from oscar.core.loading import get_class
 
-from apps.partner.utils import convert_currency, get_default_currency_code
+from apps.partner.utils import convert_currency
 
 
 FixedPrice = get_class('partner.prices', 'FixedPrice')
@@ -10,24 +13,29 @@ FixedPrice = get_class('partner.prices', 'FixedPrice')
 
 class Default(CoreDefault):
     def convert_currency(self, stockrecord, prices):
-        base_currency = self.request.basket.currency or get_default_currency_code()
-        price_excl_tax = convert_currency(stockrecord.price_currency, base_currency, prices.excl_tax)
+        basket_currency = self.request.basket.currency or settings.OSCAR_DEFAULT_CURRENCY
+        price_excl_tax = convert_currency(stockrecord.price_currency, basket_currency, prices.excl_tax)
         return FixedPrice(excl_tax=price_excl_tax, currency=self.request.basket.currency, tax=D(0))
 
     def pricing_policy(self, product, stockrecord):
         prices = super().pricing_policy(product, stockrecord)
-        base_currency = self.request.basket.currency or get_default_currency_code()
-        if base_currency == stockrecord.price_currency:
-            return prices
+        if stockrecord:
+            basket_currency = self.request.basket.currency or settings.OSCAR_DEFAULT_CURRENCY
+            if basket_currency == stockrecord.price_currency:
+                return prices
 
-        return self.convert_currency(stockrecord, prices)
+            return self.convert_currency(stockrecord, prices)
+        return prices
 
     def parent_pricing_policy(self, product, stockrecord):
         prices = super().parent_pricing_policy(product, stockrecord)
-        if self.request.basket.currency == stockrecord.price_currency:
-            return prices
+        if stockrecord:
+            basket_currency = self.request.basket.currency or settings.OSCAR_DEFAULT_CURRENCY
+            if basket_currency == stockrecord.price_currency:
+                return prices
 
-        return self.convert_currency(stockrecord, prices)
+            return self.convert_currency(stockrecord, prices)
+        return prices
 
 
 class Selector(object):
