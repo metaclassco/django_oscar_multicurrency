0824bcc0e735e7be027a62feb784410a93be5731
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-13 17:00:08 +0300

    Add basket currency field in order to track basket currency when basket does not have any lines.

--- apps/basket/models.py
+++ apps/basket/models.py
@@ -1,9 +1,25 @@
+from django.db import models
+
 from oscar.apps.basket.abstract_models import AbstractBasket
+from oscar.core.utils import get_default_currency
 
 
 class Basket(AbstractBasket):
+    _currency = models.CharField(max_length=3, default=get_default_currency)
+
+    @property
+    def currency(self):
+        return self._currency
+
+    @currency.setter
+    def currency(self, value):
+        self._currency = value
+
     def change_currency(self, currency):
-        for line in self.lines.all():
+        self.currency = currency
+        self.save()
+
+        for line in self.all_lines():
             line.price_currency = currency
             line.save()
 
