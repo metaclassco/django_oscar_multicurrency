6d486f1b0fdd848ebb5019b7d7adef37ce052584
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-13 13:27:13 +0300

    If basket currency not set - fallback to default currency in partner strategy.

--- apps/partner/strategy.py
+++ apps/partner/strategy.py
@@ -1,7 +1,8 @@
+from decimal import Decimal as D
 from oscar.apps.partner.strategy import Default as CoreDefault
 from oscar.core.loading import get_class
 
-from apps.partner.utils import convert_currency
+from apps.partner.utils import convert_currency, get_default_currency_code
 
 
 FixedPrice = get_class('partner.prices', 'FixedPrice')
@@ -9,14 +10,14 @@ FixedPrice = get_class('partner.prices', 'FixedPrice')
 
 class Default(CoreDefault):
     def convert_currency(self, stockrecord, prices):
-        price_excl_tax = convert_currency(
-            stockrecord.price_currency, self.request.basket.currency, prices.excl_tax
-        )
-        return FixedPrice(excl_tax=price_excl_tax, currency=self.request.basket.currency)
+        base_currency = self.request.basket.currency or get_default_currency_code()
+        price_excl_tax = convert_currency(stockrecord.price_currency, base_currency, prices.excl_tax)
+        return FixedPrice(excl_tax=price_excl_tax, currency=self.request.basket.currency, tax=D(0))
 
     def pricing_policy(self, product, stockrecord):
         prices = super().pricing_policy(product, stockrecord)
-        if self.request.basket.currency == stockrecord.price_currency:
+        base_currency = self.request.basket.currency or get_default_currency_code()
+        if base_currency == stockrecord.price_currency:
             return prices
 
         return self.convert_currency(stockrecord, prices)
