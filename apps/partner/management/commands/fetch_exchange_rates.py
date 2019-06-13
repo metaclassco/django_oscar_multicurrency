9d73008eba54eff2f19c2175e1032cafc992a76f
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-13 16:21:25 +0300

    Refactor fetch exchange rates management command to work with currency codes and without currency model.

--- apps/partner/management/commands/fetch_exchange_rates.py
+++ apps/partner/management/commands/fetch_exchange_rates.py
@@ -1,15 +1,16 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from apps.partner import utils
-from apps.partner.models import Currency, ExchangeRate
+from apps.partner.models import ExchangeRate
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for base_currency in Currency.objects.all():
-            currencies = ','.join(Currency.objects.exclude(code=base_currency.code).values_list('code', flat=True))
-            rates = utils.fetch_exchange_rates(base_currency.code, currencies=currencies)
+        for base_currency in settings.OSCAR_CURRENCIES:
+            currencies = list(settings.OSCAR_CURRENCIES).copy()
+            currencies.remove(base_currency)
+            rates = utils.fetch_exchange_rates(base_currency, currencies=','.join(currencies))
             for rate_code, rate_value in rates['rates'].items():
-                currency = Currency.objects.get(code=rate_code)
-                ExchangeRate.objects.create(base_currency=base_currency, currency=currency, value=rate_value)
-                self.stdout.write('Fetched rate %s to %s' % (base_currency.code, rate_code))
+                ExchangeRate.objects.create(base_currency=base_currency, currency=rate_code, value=rate_value)
+                self.stdout.write('Fetched rate %s to %s' % (base_currency, rate_code))
