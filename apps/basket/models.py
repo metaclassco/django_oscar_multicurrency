b12c4f4755033f9d26873528c1d57cec45c2efee
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-13 13:22:17 +0300

    Replace basket line price currency field with FK to currency model, override basket model save method in order to set initial currency.

--- apps/basket/models.py
+++ apps/basket/models.py
@@ -1,10 +1,23 @@
 from django.db import models
-from oscar.apps.basket.abstract_models import AbstractBasket
+from oscar.apps.basket.abstract_models import AbstractBasket, AbstractLine
+
+from apps.partner.utils import get_default_currency
 
 
 class Basket(AbstractBasket):
     currency = models.ForeignKey(
         'partner.Currency', on_delete=models.CASCADE, related_name='baskets', null=True, blank=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.currency:
+            self.currency = get_default_currency()
+        super().save(*args, **kwargs)
+
+
+class Line(AbstractLine):
+    price_currency = models.ForeignKey(
+        'partner.Currency', on_delete=models.CASCADE, related_name='basket_lines', null=True, blank=True
     )
 
 
