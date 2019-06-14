c8d682842a7edd2d931e84788f8dba3016c43541
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-14 10:04:43 +0300

    Add currency to the offer benefit model.

--- apps/offer/models.py
+++ apps/offer/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+
+from oscar.apps.offer.abstract_models import AbstractBenefit
+from oscar.core.utils import get_default_currency
+
+
+class Benefit(AbstractBenefit):
+    currency = models.CharField(max_length=3, default=get_default_currency)
+
+
 from oscar.apps.offer.models import *  # noqa isort:skip
