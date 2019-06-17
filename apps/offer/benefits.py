601fbd7e4b9284e7307b6347f5be2fc59e1ed2bd
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-17 13:07:59 +0300

    Convert benefit value in to the basket currency before applying absolute discount benefit.

--- apps/offer/benefits.py
+++ apps/offer/benefits.py
@@ -1,7 +1,13 @@
+from decimal import Decimal as D
+
 from oscar.apps.offer.benefits import (
     AbsoluteDiscountBenefit, FixedPriceBenefit, ShippingAbsoluteDiscountBenefit, ShippingFixedPriceBenefit)
+from oscar.apps.offer.benefits import apply_discount
+from oscar.apps.offer.conditions import CoverageCondition, ValueCondition
+from oscar.apps.offer.results import ZERO_DISCOUNT, BasketDiscount
 from oscar.core.loading import get_class
 from oscar.templatetags.currency_filters import currency
+from apps.partner.utils import convert_currency
 
 range_anchor = get_class('offer.utils', 'range_anchor')
 
@@ -18,6 +24,12 @@ class CurrencyAwareAbsoluteDiscountBenefit(AbsoluteDiscountBenefit):
         return self._description % {
             'value': currency(self.value, self.currency),
             'range': range_anchor(self.range)}
+
+    def apply(self,  basket, condition, offer, **kwargs):
+        if basket.currency != self.currency:
+            discount_amount = kwargs.get('discount_amount', self.value)
+            kwargs['discount_amount'] = convert_currency(self.currency, basket.currency, discount_amount)
+        return super().apply(basket, condition, offer, **kwargs)
 
 
 class CurrencyAwareFixedPriceBenefit(FixedPriceBenefit):
