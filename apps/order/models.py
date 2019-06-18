2f59c3798ad01c7134f02b68f7e4633267ad1179
Alexander Gaevsky <sasha@sasha0.ru>
2019-06-18 14:14:47 +0300

    Implement order properties to indicate totals in default currency.

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,1 +1,32 @@
+from django.conf import settings
+
+from oscar.apps.order.abstract_models import AbstractOrder
+
+from apps.partner.utils import convert_currency
+
+
+class Order(AbstractOrder):
+    @property
+    def is_in_default_currency(self):
+        return self.currency == settings.OSCAR_DEFAULT_CURRENCY
+
+    def convert_to_default_currency(self, property_name):
+        value = getattr(self, property_name, '')
+        if not self.is_in_default_currency and value:
+            return convert_currency(self.currency, settings.OSCAR_DEFAULT_CURRENCY, value)
+        return value
+
+    @property
+    def total_incl_tax_in_default_currency(self):
+        return self.convert_to_default_currency('total_incl_tax')
+
+    @property
+    def shipping_incl_tax_in_default_currency(self):
+        return self.convert_to_default_currency('shipping_incl_tax')
+
+    @property
+    def shipping_excl_tax_in_default_currency(self):
+        return self.convert_to_default_currency('shipping_excl_tax')
+
+
 from oscar.apps.order.models import *  # noqa isort:skip
